9ffd60d7fea6b1f4d658d15d8cf844cb27dfb31b
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-06-18 10:15:44 -0300

    chore: add env variables to env validate file

--- src/shared/lib/env.ts
+++ src/shared/lib/env.ts
@@ -3,6 +3,11 @@ import z from "zod"
 const envSchema = z.object({
 	PORT: z.coerce.number().default(3333),
 	DATABASE_URL: z.string().min(1),
+	POSTGRES_USER: z.string().min(1),
+	POSTGRES_PASSWORD: z.string().min(1),
+	POSTGRES_DB: z.string().min(1),
+	POSTGRES_PORT: z.coerce.number().default(5432),
+	POSTGRES_HOST: z.string().min(1).default("localhost"),
 })
 
 export const env = envSchema.parse(process.env)
