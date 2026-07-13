7a7040ad369a34eec28a7b21730a1092c08fa0d8
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-07-13 16:12:25 -0300

    fix: remove optional property modifier of update project dto

--- src/modules/projects/dto/update-project.dto.ts
+++ src/modules/projects/dto/update-project.dto.ts
@@ -2,8 +2,8 @@ export type UpdateProjectDTO = {
 	id: number
 	name: string
 	description: string
-	gitHubUrl?: string
+	gitHubUrl: string | null
 	publishedAt: Date
-	thumbnailUrl?: string
+	thumbnailUrl: string | null
 	stackIds: number[]
 }
