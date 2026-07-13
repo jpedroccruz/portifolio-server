01b166c059857b764727e222ce0d3114b0740a04
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-07-13 20:07:09 -0300

    fix: make prisma stack repository

--- src/modules/stacks/factories/makePrismaStackRepository.ts
+++ src/modules/stacks/factories/makePrismaStackRepository.ts
@@ -1,8 +1,6 @@
 import { prisma } from "../../../shared/prisma/prisma-client.js"
 import { PrismaStackRepository } from "../repositories/prisma-stack.repository.js"
-import { ListStacksService } from "../services/list-stacks.service.js"
 
 export function makePrismaStackRepository() {
-	const repository = new PrismaStackRepository(prisma)
-	return new ListStacksService(repository)
+	return new PrismaStackRepository(prisma)
 }
