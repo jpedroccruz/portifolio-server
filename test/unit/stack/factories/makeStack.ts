6fd5ef86f5e4e852cb1a5205410d0d1e6dd074d0
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-07-14 16:30:49 -0300

    refactor: add retrun type to makeStack

--- test/unit/stack/factories/makeStack.ts
+++ test/unit/stack/factories/makeStack.ts
@@ -1,6 +1,7 @@
 import { uuid } from "zod"
+import type { CreateStackDTO } from "../../../../src/modules/stacks/dto/create-stack.dto"
 
-export function makeStack() {
+export function makeStack(): CreateStackDTO {
 	return {
 		name: `Ts-${uuid}`,
 		iconUrl: "https://commons.wikimedia.org/wiki/File:Typescript_logo_2020.svg",
