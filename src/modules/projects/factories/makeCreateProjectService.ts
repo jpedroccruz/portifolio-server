94478b47ebfb7042c4a795a56b30f87602358423
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-07-14 16:30:26 -0300

    fix: add stack repository as DI of make create/update project services

--- src/modules/projects/factories/makeCreateProjectService.ts
+++ src/modules/projects/factories/makeCreateProjectService.ts
@@ -1,6 +1,10 @@
+import type { StackRepository } from "../../stacks/repositories/stack.repository.js"
 import type { ProjectRepository } from "../repositories/project.repository.js"
 import { CreateProjectService } from "../services/create-project.service.js"
 
-export function makeCreateProjectService(repository: ProjectRepository) {
-	return new CreateProjectService(repository)
+export function makeCreateProjectService(
+	projectRepository: ProjectRepository,
+	stackRepository: StackRepository,
+) {
+	return new CreateProjectService(projectRepository, stackRepository)
 }
--- src/modules/projects/factories/makeUpdateProjectService.ts
+++ src/modules/projects/factories/makeUpdateProjectService.ts
@@ -1,6 +1,10 @@
+import type { StackRepository } from "../../stacks/repositories/stack.repository.js"
 import type { ProjectRepository } from "../repositories/project.repository.js"
 import { UpdateProjectService } from "../services/update-project.service.js"
 
-export function makeUpdateProjectService(repository: ProjectRepository) {
-	return new UpdateProjectService(repository)
+export function makeUpdateProjectService(
+	projectRepository: ProjectRepository,
+	stackRepository: StackRepository,
+) {
+	return new UpdateProjectService(projectRepository, stackRepository)
 }
