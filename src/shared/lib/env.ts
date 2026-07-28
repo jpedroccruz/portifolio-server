06228337c5446a25d16ee14514aeb7159a1c6c3f
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-07-28 19:58:00 -0300

    feat: add jwt secret to env variables

--- src/shared/lib/env.ts
+++ src/shared/lib/env.ts
@@ -2,14 +2,9 @@ import z from "zod"
 
 const envSchema = z.object({
 	PORT: z.coerce.number().default(3333),
-	DATABASE_URL: z.string().min(1),
-	POSTGRES_USER: z.string().min(1),
-	POSTGRES_PASSWORD: z.string().min(1),
-	POSTGRES_DB: z.string().min(1),
-	POSTGRES_PORT: z.coerce.number().default(5432),
-	POSTGRES_HOST: z.string().min(1).default("localhost"),
 	EMAIL: z.email().min(1),
 	MAIL_PROVIDER_KEY: z.string().min(1),
+	JWT_SECRET: z.string().min(1),
 })
 
 export const env = envSchema.parse(process.env)
